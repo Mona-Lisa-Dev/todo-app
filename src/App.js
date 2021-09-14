3e45f972923acb482ab5d23a0d43a14bf8633cac
Olena Tsemko <olenatsemko@Olenas-MacBook-Pro.local>
2021-09-14 12:20:08 +0300

    add readme and code splitting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import { useEffect } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
-// import Homepage from 'pages/Homepage';
-import RegisterPage from 'pages/RegisterPage';
-import LoginPage from 'pages/LoginPage';
-import TodosPage from 'pages/TodosPage';
 import AppBar from 'components/AppBar';
 import Container from 'components/Container';
-
 import PrivateRoute from 'components/PrivateRoute';
 import PublicRoute from 'components/PublicRoute';
 
@@ -17,6 +12,16 @@ import { getIsAuthorized } from 'redux/auth/auth-selectors';
 
 import routes from 'routes';
 import './scss/_main.scss';
+
+const LoginPage = lazy(() =>
+  import('./pages/LoginPage' /* webpackChunkName: "LoginPage" */),
+);
+const RegisterPage = lazy(() =>
+  import('./pages/RegisterPage' /* webpackChunkName: "RegisterPage" */),
+);
+const TodosPage = lazy(() =>
+  import('./pages/TodosPage' /* webpackChunkName: "TodosPage" */),
+);
 
 const App = () => {
   const isAuthorized = useSelector(getIsAuthorized);
@@ -27,39 +32,40 @@ const App = () => {
     <>
       <AppBar />
       <Container>
-        <Switch>
-          {/* <PublicRoute path={routes.home} exact component={Homepage} /> */}
-          <PrivateRoute
-            path={routes.todos}
-            redirectTo={routes.login}
-            component={TodosPage}
-          />
-          <PublicRoute
-            path={routes.signup}
-            redirectTo={routes.todos}
-            restricted
-            component={RegisterPage}
-          />
-          <PublicRoute
-            path={routes.login}
-            redirectTo={routes.todos}
-            restricted
-            component={LoginPage}
-          />
+        <Suspense fallback={null}>
+          <Switch>
+            <PrivateRoute
+              path={routes.todos}
+              redirectTo={routes.login}
+              component={TodosPage}
+            />
+            <PublicRoute
+              path={routes.signup}
+              redirectTo={routes.todos}
+              restricted
+              component={RegisterPage}
+            />
+            <PublicRoute
+              path={routes.login}
+              redirectTo={routes.todos}
+              restricted
+              component={LoginPage}
+            />
 
-          <Route
-            path={routes.home}
-            render={props =>
-              isAuthorized ? (
-                <Redirect to={routes.todos} />
-              ) : (
-                <Redirect to={routes.login} />
-              )
-            }
-          />
+            <Route
+              path={routes.home}
+              render={props =>
+                isAuthorized ? (
+                  <Redirect to={routes.todos} />
+                ) : (
+                  <Redirect to={routes.login} />
+                )
+              }
+            />
 
-          <Redirect to={routes.home} />
-        </Switch>
+            <Redirect to={routes.home} />
+          </Switch>
+        </Suspense>
       </Container>
     </>
   );
